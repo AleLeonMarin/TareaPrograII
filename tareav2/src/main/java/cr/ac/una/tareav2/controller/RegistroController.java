987f4dba4e95e3c81b3c48a016d8a05a1978ff0f
AleLeonMarin <103346986+AleLeonMarin@users.noreply.github.com>
2023-05-01 22:26:15 +0100

    Update RegistroController.java

--- tareav2/src/main/java/cr/ac/una/tareav2/controller/RegistroController.java
+++ tareav2/src/main/java/cr/ac/una/tareav2/controller/RegistroController.java
@@ -7,6 +7,7 @@ package cr.ac.una.tareav2.controller;
 import cr.ac.una.tareav2.util.FlowController;
 import cr.ac.una.tareav2.util.Mensaje;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
@@ -19,6 +20,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -40,13 +42,20 @@ public class RegistroController extends Controller implements Initializable {
     @FXML
     private MFXTextField txfEmail;
     @FXML
-    private MFXDatePicker dpDate;
-    @FXML
     private MFXButton btnCancel;
     @FXML
     private MFXButton btnLogInView;
     @FXML
     private AnchorPane rootRegister;
+    @FXML
+    private MFXCheckbox checkboxCliente;
+    @FXML
+    private MFXCheckbox checkboxAdmin;
+
+    @FXML
+    private MFXTextField txfDate;
+    @FXML
+    private MFXTextField psfClave;
 
     /**
      * Initializes the controller class.
@@ -54,76 +63,85 @@ public class RegistroController extends Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void onActionBtnLogIn(ActionEvent event) {
-        
-        try{
-            
-            if(txfName.getText() == null || txfName.getText().isBlank()){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Nombre",getStage(),"Es necesario digitar un nombre para registrar");
-                
-            }else if(txfLastName.getText() == null || txfLastName.getText().isBlank()){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Apellidos",getStage(),"Es necesario digitar apellidos para registrar");
-                
-            }else if(txfID.getText() == null || txfID.getText().isBlank() ){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Cedula",getStage(),"Es necesario digitar una cedula para registrar");
-                
-            }else if(txfNumber.getText() == null || txfNumber.getText().isBlank()){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Numero telefonico",getStage(),"Es necesario digitar un numero telefonico para registrar");
-                
-            }else if(txfEmail.getText() == null || txfEmail.getText().isBlank()){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Correo",getStage(),"Es necesario digitar un correo electronico para registrar");
-                
-            }else if(dpDate.getText() == null || dpDate.getText().isBlank() ){
-                
-                new Mensaje().show(AlertType.ERROR,"Validacion de Fecha de Nacimiento",getStage(),"Es necesario digitar una fecha de nacimiento para registrar");
-                
-            }else{
-                
-                 FlowController.getInstance().goMain();
-                
+
+        try {
+
+            if (txfName.getText() == null || txfName.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Nombre", getStage(), "Es necesario digitar un nombre para registrar");
+
+            } else if (txfLastName.getText() == null || txfLastName.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Apellidos", getStage(), "Es necesario digitar apellidos para registrar");
+
+            } else if (txfID.getText() == null || txfID.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Cedula", getStage(), "Es necesario digitar una cedula para registrar");
+
+            } else if (txfNumber.getText() == null || txfNumber.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Numero telefonico", getStage(), "Es necesario digitar un numero telefonico para registrar");
+
+            } else if (txfEmail.getText() == null || txfEmail.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Correo", getStage(), "Es necesario digitar un correo electronico para registrar");
+
+            } else if (txfDate.getText() == null || txfDate.getText().isBlank()) {
+
+                new Mensaje().show(AlertType.ERROR, "Validacion de Fecha de Nacimiento", getStage(), "Es necesario digitar una fecha de nacimiento para registrar");
+
+            } else if (psfClave.getText() == null || psfClave.getText().isBlank()) {
+                new Mensaje().show(AlertType.ERROR, "Validacion de clave de ingreso", getStage(), "Es necesario ingresar una clave");
+            } else {
+
+                FlowController.getInstance().goViewInWindow("LogInVIew");
+
             }
-        }catch(Exception ex){
-        
-            Logger.getLogger(LogInViewController.class.getName()).log(Level.SEVERE,"Error ingresado", ex );
-            new Mensaje().show(Alert.AlertType.ERROR,"Registro", getStage(),"Error al sistema ingresado");
-        
-        
+
+        } catch (Exception ex) {
+
+            Logger.getLogger(RegistroController.class.getName()).log(Level.SEVERE, "Error ingresado", ex);
+            new Mensaje().show(Alert.AlertType.ERROR, "Registro", getStage(), "Error al sistema ingresado");
         }
-        
-        
+
     }
 
     @FXML
     private void onActionBtnCancel(ActionEvent event) {
-        
+
         txfName.clear();
         txfLastName.clear();
         txfID.clear();
         txfNumber.clear();
         txfEmail.clear();
-        dpDate.clear();
+        txfDate.clear();
+        psfClave.clear();
+        FlowController.getInstance().goViewInWindow("LogInView");
+        ((Stage) btnCancel.getScene().getWindow()).close();
     }
-    
+
     @FXML
     private void onActionBtnLogInView(ActionEvent event) {
-        
+
         FlowController.getInstance().goViewInWindow("LogInView");
-        
+        ((Stage) btnLogIn.getScene().getWindow()).close();
     }
 
     @Override
     public void initialize() {
-        
+
     }
 
-    
-    
+    @FXML
+    private void onActionCheckBoxCliente(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionCheckBoxAdmin(ActionEvent event) {
+    }
+
 }
