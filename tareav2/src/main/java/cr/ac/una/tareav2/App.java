341af7840bc02533e201b9552dbc1e0b21a2ee42
Daniel Araya <darayaroma@gmail.com>
2023-05-02 02:49:29 -0600

    [rebase]

--- tareav2/src/main/java/cr/ac/una/tareav2/App.java
+++ tareav2/src/main/java/cr/ac/una/tareav2/App.java
@@ -2,11 +2,7 @@ package cr.ac.una.tareav2;
 
 import cr.ac.una.tareav2.util.FlowController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import javafx.scene.image.Image;
 
@@ -15,12 +11,10 @@ import javafx.scene.image.Image;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        
-        FlowController.getInstance().InitializeFlow(stage,  null);
+
+        FlowController.getInstance().InitializeFlow(stage, null);
         stage.getIcons().add(new Image("cr/ac/una/tareav2/resources/PrincipalBackGround.jpg"));
         stage.setTitle("Tours");
         FlowController.getInstance().goViewInWindow("PaginaPrincipal");
