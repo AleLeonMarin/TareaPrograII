60144b9bf85cb15fa5e863d25f27e5a42617be35
Eslyn19 <andresjara479@gmail.com>
2024-02-29 21:26:06 -0600

    Update view

--- src/main/java/cr/ac/una/tarea/controller/ManCuentasViewController.java
+++ src/main/java/cr/ac/una/tarea/controller/ManCuentasViewController.java
@@ -4,28 +4,29 @@
  */
 package cr.ac.una.tarea.controller;
 
-import cr.ac.una.tarea.util.FlowController;
-import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
- * @author aletr
+ * @author OUTLET
  */
-public class ProfeViewController extends Controller implements Initializable {
+public class ManCuentasViewController extends Controller implements Initializable {
 
     @FXML
-    private MFXButton btnManCuentas;
+    private AnchorPane root;
     @FXML
-    private MFXButton btmManCoope;
+    private MFXComboBox<String> comboBox;
     @FXML
-    private BorderPane root;
+    private MFXTextField txfNomCuentas;
+    @FXML
+    private MFXTextField txfId;
 
     /**
      * Initializes the controller class.
@@ -38,15 +39,6 @@ public class ProfeViewController extends Controller implements Initializable {
     @Override
     public void initialize() {
         
-    }
-
-    @FXML
-    private void onActionBtnManCuentas(ActionEvent event) {
-        FlowController.getInstance().goView("ManCuentasView");
-    }
-
-    @FXML
-    private void onActionBtmManCoope(ActionEvent event) {
     }
     
 }
