947d03eb67dec2c6502e73d8110124186e398569
Alejandro Leon <aleleonmarin01@gmail.com>
2024-03-21 12:20:16 -0600

    [ADD] CSS

---
+++ src/main/java/cr/ac/una/tarea/Main.java
@@ -0,0 +1,6 @@
+package cr.ac.una.tarea;
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
--- src/main/java/cr/ac/una/tarea/controller/EditarAsociadoController.java
+++ src/main/java/cr/ac/una/tarea/controller/EditarAsociadoController.java
@@ -6,6 +6,7 @@ import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -56,6 +57,9 @@ public class EditarAsociadoController extends Controller implements Initializabl
 
     @Override
     public void initialize() {
+        Image image = new Image(getClass().getResourceAsStream("../resources/Person.png"));
+
+        imgFoto.setImage(image);
 
     }
 
