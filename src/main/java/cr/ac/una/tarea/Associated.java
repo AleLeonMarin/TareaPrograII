079236810446bb8fd08d83e762302698c7471a7d
AleLeonMarin <aleleonmarin01@gmail.com>
2024-03-25 19:18:25 -0600

    [IMP] CSS

--- src/main/java/cr/ac/una/tarea/Associated.java
+++ src/main/java/cr/ac/una/tarea/Associated.java
@@ -1,4 +1,54 @@
 package cr.ac.una.tarea;
 
 public class Associated {
+
+    public String name;
+    public int age;
+
+    public String Folio;
+
+    public String Photo;
+
+    public Associated(String name, int age, String folio, String photo) {
+        this.name = name;
+        this.age = age;
+        Folio = folio;
+        Photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFolio() {
+        return Folio;
+    }
+
+    public void setFolio(String folio) {
+        Folio = folio;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String photo) {
+        Photo = photo;
+    }
+
+    public void createFolio(){
+
+    }
 }
