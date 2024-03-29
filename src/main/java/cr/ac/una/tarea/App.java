0a24604e2461e8400f063b13bcf1dbf2907843d7
AleLeonMarin <aleleonmarin01@gmail.com>
2024-03-28 23:01:02 -0600

    [UPDATED]

--- src/main/java/cr/ac/una/tarea/App.java
+++ src/main/java/cr/ac/una/tarea/App.java
@@ -2,27 +2,14 @@ package cr.ac.una.tarea;
 
 import cr.ac.una.tarea.util.FlowController;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javafx.scene.image.Image;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    private static Scene scene;
-
-
 
     @Override
     public void start(Stage stage) throws IOException {
