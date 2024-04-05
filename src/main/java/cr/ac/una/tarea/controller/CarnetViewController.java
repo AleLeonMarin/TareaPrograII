eb7c72f30fa9253e05d35de2d71340f48bdb884c
AleLeonMarin <aleleonmarin01@gmail.com>
2024-04-04 20:48:11 -0600

    [UPDATED]

--- src/main/java/cr/ac/una/tarea/controller/CarnetViewController.java
+++ src/main/java/cr/ac/una/tarea/controller/CarnetViewController.java
@@ -1,20 +1,38 @@
 package cr.ac.una.tarea.controller;
 
+import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import cr.ac.una.tarea.model.Associated;
+import io.github.palexdev.materialfx.utils.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 
 public class CarnetViewController extends Controller implements Initializable {
 
-    ArrayList<Associated> asociados = new ArrayList<>();
+    //ArrayList<Associated> asociados = new ArrayList<>();
 
+    Associated asociado = new Associated();
     @FXML
     private AnchorPane root;
     
@@ -35,12 +53,51 @@ public class CarnetViewController extends Controller implements Initializable {
 
     @Override
     public void initialize() {
-        // Empty
+
+        
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // Empyt
+        createPdf();
+    }
+
+    public void createPdf(){
+
+        try {
+            Document documento = new Document(PageSize.A4);
+            PdfWriter.getInstance(documento, new FileOutputStream("Carnet" + asociado.getName() + asociado.getFolio() + ".pdf"));
+            documento.open();
+
+            WritableImage image = root.snapshot(new SnapshotParameters(), null);
+            File output = new File("Carnet" + asociado.getName() + asociado.getFolio() + ".png");
+
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", output);
+            float pageWidth = documento.getPageSize().getWidth();
+            float pageHeight = documento.getPageSize().getHeight();
+            BufferedImage bufferedImage = ImageIO.read(output);
+            float imageWidth = bufferedImage.getWidth();
+            float imageHeight = bufferedImage.getHeight();
+            float scale = Math.min(pageWidth / imageWidth, pageHeight / imageHeight);
+
+            Image imagen = Image.getInstance(output.getPath());
+            imagen.scaleToFit(imageWidth * scale, imageHeight * scale);
+            imagen.setAlignment(Image.ALIGN_CENTER);
+            documento.add(imagen);
+
+            documento.close();
+
+            Desktop desktop = Desktop.getDesktop();
+
+            File file = new File("Carnet" + asociado.getName() + asociado.getFolio() + ".pdf");
+            desktop.open(file);
+
+        } catch (FileNotFoundException | DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/main/java/cr/ac/una/tarea/controller/ImpresCarnetController.java
+++ src/main/java/cr/ac/una/tarea/controller/ImpresCarnetController.java
@@ -2,15 +2,32 @@ package cr.ac.una.tarea.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.utils.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
+import javafx.scene.image.WritableImage;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import cr.ac.una.tarea.model.Associated;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 public class ImpresCarnetController extends Controller implements Initializable {
 
     @FXML
@@ -21,15 +38,18 @@ public class ImpresCarnetController extends Controller implements Initializable 
 
     @FXML
     private MFXTextField txtNombre;
+    
+    CarnetViewController carnet = new CarnetViewController();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Document doc = new Document();
+       
     }
 
     @FXML
     void onActionBtnGenerar(ActionEvent event) {
-        // TODO
+        carnet.createPdf();
+
     }
 
     @Override
